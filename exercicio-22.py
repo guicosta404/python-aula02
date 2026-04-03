6c0eb2e0ffb671b6849a88bbc61f86dcfb58df05
Luiz Guilherme <lu.costa1402@gmail.com>
2026-04-03 00:33:56 -0300

    Resolução do exercício 22 da aula 2 de python

---
+++ exercicio-22.py
@@ -0,0 +1,9 @@
+frase = input("Digite uma frase: ").lower()
+if isinstance(frase, str):
+    formatado = frase.replace(" ", "").lower()
+    if formatado == formatado[::-1]:
+        print("É um palíndromo.")
+    else:
+        print("Não é um palíndromo.")
+else:
+    print("Entrada inválida. Por favor, digite uma palavra ou frase.")
